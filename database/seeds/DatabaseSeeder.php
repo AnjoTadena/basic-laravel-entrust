879c8294c14d40fd5b8d259c884250d0f8e0a937
Anjo Tadena <anjo@coderepubliq.com>
2018-08-14 18:04:19 +0800

    Add database seeder.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\Permission;
+use App\Role;
+use App\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,5 +17,25 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        DB::table('users')->delete();
+
+        //1) Create Admin Role
+        $role = ['name' => 'admin', 'display_name' => 'Admin', 'description' => 'Full Permission'];
+
+        $role = Role::create($role);
+
+        //2) Set Role Permissions
+        // Get all permission, swift through and attach them to the role
+        $permission = Permission::get();
+
+        foreach ($permission as $key => $value) $role->attachPermission($value);
+
+        //3) Create Admin User
+        $user = ['name' => 'Admin User', 'email' => 'adminuser@test.com', 'password' => Hash::make('adminpwd')];
+
+        $user = User::create($user);
+
+        //4) Set User Role
+        $user->attachRole($role);
     }
 }
