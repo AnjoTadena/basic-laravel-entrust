0572537fb69206d3f89feda687c037468e05c2e0
Anjo Tadena <anjo@coderepubliq.com>
2018-08-14 17:49:57 +0800

    Setup job fillable attributes.

--- app/Job.php
+++ app/Job.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Job extends Model
 {
-    //
+
+    /**
+     * The attributes that are mass assignable
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title',
+        'description',
+        'salary',
+        'added_by',
+        'updated_by'
+    ];
 }
